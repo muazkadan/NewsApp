89edfb2981a30e3b8ddf3a1cd4302ee99149cfdb
muazkadan <muaz.kadan@gmail.com>
2022-09-13 22:15:12 +0300

    add AppBar to categories page

--- app/src/main/java/net/mouazkaadan/inshort/presentation/categories/CategoriesScreen.kt
+++ app/src/main/java/net/mouazkaadan/inshort/presentation/categories/CategoriesScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -11,16 +12,26 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import net.mouazkaadan.inshort.R
 import net.mouazkaadan.inshort.data.local.Categories
 import net.mouazkaadan.inshort.data.model.CategoryModel
 import net.mouazkaadan.inshort.presentation.Screen
@@ -29,20 +40,56 @@ import net.mouazkaadan.inshort.presentation.Screen
  * @author muaz
  * Created on 9/8/2022.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoriesScreen(navController: NavController) {
     val gridState = rememberLazyGridState()
-    LazyVerticalGrid(
-        modifier = Modifier.fillMaxSize(),
-        state = gridState,
-        columns = GridCells.Adaptive(140.dp)
+    val topAppBarState = rememberTopAppBarState()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(topAppBarState)
+
+    Scaffold(
+        topBar = {
+            ShortTopAppBar(scrollBehavior)
+        }
     ) {
-        items(Categories.categoriesList) { category ->
-            CategoryCard(category = category) {
-                navController.navigate(Screen.NewsDetailsScreen.withArgs(category.value))
+        LazyVerticalGrid(
+            modifier = Modifier
+                .fillMaxSize()
+                .nestedScroll(scrollBehavior.nestedScrollConnection)
+                .padding(it),
+            state = gridState,
+            columns = GridCells.Adaptive(140.dp)
+        ) {
+            items(Categories.categoriesList) { category ->
+                CategoryCard(category = category) {
+                    navController.navigate(Screen.NewsDetailsScreen.withArgs(category.value))
+                }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ShortTopAppBar(scrollBehavior: TopAppBarScrollBehavior) {
+    CenterAlignedTopAppBar(
+        modifier = Modifier.height(32.dp),
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            scrolledContainerColor = MaterialTheme.colorScheme.primary
+        ),
+        title = {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.labelLarge,
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior
+    )
 }
 
 @Composable
